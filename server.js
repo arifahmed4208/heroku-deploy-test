f66b8daf31b2577d11bf9c7f46b11e566ab8d347
arif.ahmed <arif.ahmed@cgi.com>
2020-12-18 17:50:46 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const expres = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/test-app'));
+
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/test-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
